5d15975e34ca67e582b67ee3cb9a44e3299d05b3
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2014-09-24 15:43:22 +0200

    dump the container if it is started

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -8,9 +8,21 @@ import (
 
 var endpoint = flag.String("socket", "unix:///var/run/docker.sock", "the docker socket to use")
 
+type eventListener chan *docker.APIEvents
+
+type containerEvent string
+
+const (
+	evtStart   containerEvent = "start"
+	evtStop                   = "stop"
+	evtDie                    = "die"
+	evtDestroy                = "destroy"
+)
+
 func main() {
 	flag.Parse()
 	client, _ := docker.NewClient(*endpoint)
+	//client.AddEventListener(listener)
 	imgs, _ := client.ListImages(true)
 	for _, img := range imgs {
 		fmt.Println("ID: ", img.ID)
@@ -19,5 +31,18 @@ func main() {
 		fmt.Println("Size: ", img.Size)
 		fmt.Println("VirtualSize: ", img.VirtualSize)
 		fmt.Println("ParentId: ", img.ParentId)
+	}
+
+	listener := make(eventListener)
+	client.AddEventListener(listener)
+	for e := range listener {
+		evt := containerEvent(e.Status)
+		if evt == evtStart {
+			cnt, err := client.InspectContainer(e.ID)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Started: %#v\n", *cnt)
+		}
 	}
 }
